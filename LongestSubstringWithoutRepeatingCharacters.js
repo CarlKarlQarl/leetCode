9fd8bedbc049f4cf9e750d8a3de2e9458c7dc2dd
CarlKarlQarl <karlmatthes@gmail.com>
2020-09-30 15:53:02 -0600

    Added LongestSubstringWithoutRepeatingCharacters

---
+++ LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+lengthOfLongestSubstring = (string) => {
+    let length = string.length
+    let count = 0
+    let best = []
+    let duplicates = new Set()
+    for (let i = 0; i < length; i++){
+        if(duplicates.has(string[i])){
+            best.push(count)
+            count = 1
+            i = 
+        } else {
+            duplicates.add(string[i])
+            count++
+        }
+        best.push(count)
+    }
+    return best.reduce((max, num) => num > max ? num : max, 0)
+};
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Two Sum - https://leetcode.com/problems/two-sum/submissions/
 
 ## In Progress
 Add Two Numbers - https://leetcode.com/problems/add-two-numbers/submissions/
+Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
